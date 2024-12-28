87736e51ae01cb839c4f5fec8014e7b1a3173a90
Nguyễn Hoàng Đức Minh <nhducminhqt@gmail.com>
2024-12-28 18:15:18 +0700

    add new html to djanggo

--- site1/site1/urls.py
+++ site1/site1/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from home import views as home
 
 urlpatterns = [
+    path("",home.get_home ),
     path('admin/', admin.site.urls),
 ]
