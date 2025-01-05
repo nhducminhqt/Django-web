c9d313efacf96b0bb00e3ab6026f094baafb6db2
Nguyễn Hoàng Đức Minh <nhducminhqt@gmail.com>
2025-01-05 13:57:40 +0700

    add

--- site1/site1/settings.py
+++ site1/site1/settings.py
@@ -17,7 +17,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
+# See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/add
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-(4&c0kz_-lyx6#)l5odoac^ghsa+j6^9pi5^eb*@e39p=65xul'
